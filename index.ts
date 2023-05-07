1c086c4a02c70f54ea0046c09abff1f25e5a4fb6
Ali.Shafiee <shafiee.ali10@gmail.com>
2023-05-07 03:47:11 +0330

    Use presentaion layer method in api and send mocked url

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
-import express, {Express, Request, Response} from 'express';
+import express, {Express} from 'express';
+import UrlShortenerPresentation from './presentation/shortener';
 
 
 const app: Express = express();
 
 
-app.post('/short', )
+app.post('/short', UrlShortenerPresentation.shortenerPostApi)
 
 app.listen(8001);
--- presentation/shortener.ts
+++ presentation/shortener.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from 'express';
 import { z } from 'zod';
 
-class UrlShortenerPresentation {
+export default class UrlShortenerPresentation {
     public static async shortenerPostApi(req: Request, res: Response): Promise<void> {
         const bodyValidator = z.object({
             url: z.string().url()
         });
         const parseResult = bodyValidator.safeParse(req.body)
         if (parseResult.success) {
-            const urlShortenerResponse = { shortUrl: 'mocked url' };
-            res.status(200).send(urlShortenerResponse); 
-            
+            const urlShortenerResponse = { shortUrl: 'http://localhost.com/mocked-url' };
+            res.status(200).send(urlShortenerResponse);
+        } else {
+            res.status(404).send({ error: "Error!!!!!!!!!!!!!" });
         }
-        res.status(404).send({ error: "Error!!!!!!!!!!!!!" });
     }
 }
