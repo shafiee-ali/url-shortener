6c09e0d08bb262e0c856f631e3eccaeb4e5efa78
Ali.Shafiee <shafiee.ali10@gmail.com>
2023-05-07 06:48:11 +0330

    Before read from mongo, read from cache and add some logs for check cache hit/miss

--- application/shortner.ts
+++ application/shortner.ts
@@ -1,5 +1,6 @@
 import { ShortUrlInput, ShortUrlOutput } from "../domain/shortner";
 import ShortUrlDataAccess from "../data-access/shortener";
+import { redisClient } from "../database-connection";
 
 
 class UrlShortnerApplication {
@@ -9,11 +10,21 @@ class UrlShortnerApplication {
     }
     public static async shortUrl(input: ShortUrlInput): Promise<ShortUrlOutput> 
     {
-        let shortUrl = await ShortUrlDataAccess.findByMainUrl(input.url)
+        
+        let shortUrl: string | null | undefined = await redisClient.get(input.url);
+        if (shortUrl){
+            console.log('Hitting cache for get short url');
+            return {shortUrl}   
+        }
+        console.log('Missing cache for get short url');
+        shortUrl = await ShortUrlDataAccess.findByMainUrl(input.url)
         if (!shortUrl) {
+            console.log('Short url not exist in database');
             shortUrl = this.shortingUrlAlgorith(input.url);
             await ShortUrlDataAccess.insert(input.url, shortUrl);
         }
+        console.log('Short url exists in database but not cached');
+        redisClient.set(input.url, shortUrl);
         return {shortUrl}
     }
 }
