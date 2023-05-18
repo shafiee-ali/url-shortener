ad09ec03c085950bc230ab5ed17a6de310b23fd0
Ali.Shafiee <shafiee.ali10@gmail.com>
2023-05-18 20:04:57 +0330

    Remove all js files

--- model/short-url.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const short_url_1 = __importDefault(require("../schema/short-url"));
-const ShortUrlModel = mongoose_1.default.model('ShortUrl', short_url_1.default, 'ShortUrl');
-exports.default = ShortUrlModel;
--- schema/short-url.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const ShortUrlSchema = new mongoose_1.Schema({
-    longurl: String,
-    shortUrl: String,
-});
-exports.default = ShortUrlSchema;
